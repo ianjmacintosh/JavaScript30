93ec868f556309d68488643499067aef0de6dd19
Ian MacIntosh <ianjmacintosh@gmail.com>
2019-02-05 23:51:18 -0500

    Implement video scrubber
    This still doesn't work the way I'd like exactly -- I still need to
    make it update the video playback status as soon as the page loads.
    I don't understand why the seeker bar is in the middle at first, and
    I'd like to fix it without modifying the stylesheet.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,6 +4,10 @@ players.forEach(function initializePlayer(player) {
     const video = player.querySelector("video.viewer");
     const playButton = player.querySelector("button[title='Toggle Play']");
     const volumeSlider = player.querySelector("input[name=volume]");
+    const seeker = player.querySelector(".progress");
+    const seekerProgress = seeker.querySelector(".progress__filled");
+
+    let seeking = false;
 
     const toggleVideoPlayback = (video) => {
         if (video.paused) {
@@ -17,12 +21,33 @@ players.forEach(function initializePlayer(player) {
         video.volume = volume;
     }
 
-    const updateSeeker = (percentage) => {
+    const seekTo = (percentage) => {
         const newTime = video.duration * percentage;
         video.currentTime = newTime;
     }
 
+    const updateSeekerBar = () => {
+        console.log("It ran" + video.duration);
+        seekerProgress.style.flexBasis = `${video.currentTime / video.duration * 100}%`;
+    }
+
+    if (video.readyState === 0) {
+        video.addEventListener("load", () => updateSeekerBar);
+    } else {
+        updateSeekerBar();
+    }
     video.addEventListener("click", () => toggleVideoPlayback(video));
+    video.addEventListener("timeupdate", () => updateSeekerBar(video));
     playButton.addEventListener("click", () => toggleVideoPlayback(video));
     volumeSlider.addEventListener("input", (event) => updateVolume(event.target.value));
+    seeker.addEventListener("mousedown", () => seeking = true);
+    seeker.addEventListener("mouseup", (event) => {
+        seekTo(event.offsetX / seeker.clientWidth);
+        seeking = false;
+    });
+    seeker.addEventListener("mousemove", (event) => {
+        if (!seeking) { return false; }
+        console.log(`Seek to ${event.offsetX / seeker.clientWidth * 100}%`);
+        seekTo(event.offsetX / seeker.clientWidth);
+    });
 });
