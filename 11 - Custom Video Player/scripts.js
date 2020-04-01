f431bd378fa372b3cf25bbb21e7364a0bcdfafe5
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-04-01 16:45:49 -0600

    Get elements and store them in vars
    I'm doing some kinda squirrelly stuff with the `buttons` object and
    explicitly locating the skip buttons by the amount they skip, but it
    seemed reasonable. And I don't want to screw around with the markup if I
    can avoid it.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,14 @@
 // Get our elements
+const player = document.querySelector(".player");
+const video = player.querySelector("video");
+const progressBar = player.querySelector(".progress");
+const buttons = {
+    play: player.querySelector("button[title='Toggle Play']"),
+    skipBack: player.querySelector("button[data-skip='-10']"),
+    skipForward: player.querySelector("button[data-skip='25']")
+};
+const playbackRate = player.querySelector("input[name='playbackRate']");
+const volume = player.querySelector("input[name='volume']");
 
 // Define our functionality
 
